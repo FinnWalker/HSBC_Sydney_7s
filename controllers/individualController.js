b2d8a0b5ebb27710abaff92fd0948b56e1894047
Finn Walker <finn3walker@gmail.com>
2019-12-04 16:07:25 +0000

    Added individual get

--- controllers/individualController.js
+++ controllers/individualController.js
@@ -2,6 +2,23 @@ const individualModel = require("../models/individualModel");
 const sanitize = require("mongo-sanitize");
 
 module.exports = {
+    get: function(req, res) {
+        const individual_id = sanitize(req.body.individual_id);
+
+        if(individual_id) {
+            individualModel.find({_id: individual_id}, (err, individual) => {
+                if(err) {
+                    res.status(500).json({ message: "Error finding individual" });
+                } else if (individual) {
+                    res.status(200).json(individual);
+                } else {
+                    res.status(200).json({ message: "Individual not found" });
+                }
+            })
+        } else {
+            res.status(400).json({ message: "Please include individual_id" });
+        }
+    },
     create: function(req, res) {
       const first_name = sanitize(req.body.first_name);
       const last_name = sanitize(req.body.last_name);
