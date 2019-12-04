31e3db6ee78161004af5bcc1769d968c879c1c03
Finn Walker <finn3walker@gmail.com>
2019-12-04 16:09:38 +0000

    Individual get route fix

--- hsbc_sydney_7s.js
+++ hsbc_sydney_7s.js
@@ -28,7 +28,7 @@ app.post("/hsbc_sydney_7s/group", verifyRequest, groupController.create);
 app.post("/hsbc_sydney_7s/group/add_individual", verifyRequest, groupController.addIndividual);
 
 const individualController = require("./controllers/individualController.js");
-app.get("/hsbc_sydney_7s/individual", verifyRequest, groupController.get);
+app.get("/hsbc_sydney_7s/individual", verifyRequest, individualController.get);
 app.post("/hsbc_sydney_7s/individual", verifyRequest, individualController.create);
 
 
